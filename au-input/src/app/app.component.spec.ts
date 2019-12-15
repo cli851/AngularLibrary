75674a00d0ca2c03b68925beb50ad2eb197c79b0
cli851 <can_li@comcast.com>
2019-12-15 10:50:29 -0500

    Fixing fa-envelope test query right css element

--- au-input/src/app/app.component.spec.ts
+++ au-input/src/app/app.component.spec.ts
@@ -1,10 +1,10 @@
-import { AuFaInputComponent } from "./lib/au-fa-input/au-fa-input.component";
+import { AuInputModule } from './lib/au-input.module';
 import { TestBed, async, ComponentFixture } from "@angular/core/testing";
 
 import { AppComponent } from "./app.component";
 import { DebugElement } from "@angular/core";
 import { By } from "@angular/platform-browser";
-import { AuInputModule } from "./lib/au-input.module";
+
 
 describe("AppComponent", () => {
     let component: AppComponent,
@@ -20,14 +20,14 @@ describe("AppComponent", () => {
         }).compileComponents();
     }));
 
-    beforeEach(() => {
+    beforeEach(async(() => {
         fixture = TestBed.createComponent(AppComponent);
         component = fixture.debugElement.componentInstance;
         el = fixture.debugElement;
-        emailField = el.query(By.css("#email-field"));
+        emailField = el.query(By.css('#email-field'));
 
         fixture.detectChanges()
-    });
+    }));
 
     it("should create the app", async(() => {
         expect(component).toBeTruthy();
@@ -35,12 +35,13 @@ describe("AppComponent", () => {
 
     it("should create a font awesome email input", async(() => {
         expect(emailField).toBeTruthy();
-        expect(emailField.query(By.css('i.icon.fa.fa-envlope'))).toBeTruthy
-        expect(emailField.query(By.css('input.test-class'))).toBeTruthy
     }));
 
-    it("should include the correct email icon inside the email input", async(() => {
-        console.log(emailField.nativeElement.outerHTML);
 
-    }))
+    it("should include the correct email icon inside the email input", async(() => {
+        console.log(emailField.query(By.css('i.fa.fa-envelope')))
+        expect(emailField.query(By.css('i.fa.fa-envelope'))).toBeTruthy();
+    }));
+
+
 });
