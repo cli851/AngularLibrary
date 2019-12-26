133f51898afbc42533ac010690ac55a7f0a2bd88
cli851 <can_li@comcast.com>
2019-12-26 11:16:21 -0500

    Finishing au-masking

--- au-mask/src/app/au-mask/mask.utils.ts
+++ au-mask/src/app/au-mask/mask.utils.ts
@@ -9,7 +9,13 @@ export const  TAB = 9,
 
 export const SPECIAL_CHARACTERS = [" ", "/", "(", ")", "+", "\/", "-"];
 
+export function overWriteCharAtPosition(input: HTMLInputElement, position: number, key: string) {
 
+    const currentValue = input.value
+  
+    input.value = currentValue.slice(0, position) + key + currentValue.slice(position + 1)
+  
+  }
 
 
 
